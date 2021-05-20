698af95fca6d2cda0b9fbb8aa525365b0997a038
David Bailey <davidbailey00@outlook.com>
2021-05-20 12:14:29 +0100

    Create placeholder image outline

--- app/src/main/java/co/zimly/codelab/layouts/MainActivity.kt
+++ app/src/main/java/co/zimly/codelab/layouts/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.zimly.codelab.layouts.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,11 +45,20 @@ fun DefaultPreview() {
 
 @Composable
 fun PhotographerCard() {
-    Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        // LocalContentAlpha defines the opacity of its children
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha defines the opacity of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
