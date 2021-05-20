9fca3eb22afe2a015ca49ac7acf8c5d9f0d4fd4c
David Bailey <davidbailey00@outlook.com>
2021-05-20 12:23:48 +0100

    Apply modifiers to PhotographerCard

--- app/src/main/java/co/zimly/codelab/layouts/MainActivity.kt
+++ app/src/main/java/co/zimly/codelab/layouts/MainActivity.kt
@@ -3,14 +3,20 @@ package co.zimly.codelab.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,8 +50,15 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun PhotographerCard() {
-    Row {
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* do nothing */ })
+            .padding(16.dp)
+    ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
@@ -53,7 +66,11 @@ fun PhotographerCard() {
         ) {
             // Image goes here
         }
-        Column {
+        Column(
+            Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             // LocalContentAlpha defines the opacity of its children
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
