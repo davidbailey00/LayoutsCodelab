25bdfa4b02e6ff117d3f5f87ff6a9c08e5a41508
David Bailey <davidbailey00@outlook.com>
2021-05-22 19:41:00 +0100

    Create image list

--- app/src/main/java/co/zimly/codelab/layouts/Lists.kt
+++ app/src/main/java/co/zimly/codelab/layouts/Lists.kt
@@ -1,14 +1,19 @@
 package co.zimly.codelab.layouts
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.coil.rememberCoilPainter
 
 @Composable
 fun SimpleList() {
@@ -43,4 +48,36 @@ fun LazyList() {
 @Composable
 fun LazyListPreview() {
     LazyList()
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberCoilPainter(
+                request = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList() {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ImageListPreview() {
+    ImageList()
 }
