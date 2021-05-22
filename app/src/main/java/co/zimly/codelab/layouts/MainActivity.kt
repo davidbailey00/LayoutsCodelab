656cb28d71ad1158cf4d6ecb8857e1fc6bc8fdd5
David Bailey <davidbailey00@outlook.com>
2021-05-22 11:00:57 +0100

    Use Material Components

--- app/src/main/java/co/zimly/codelab/layouts/MainActivity.kt
+++ app/src/main/java/co/zimly/codelab/layouts/MainActivity.kt
@@ -3,11 +3,15 @@ package co.zimly.codelab.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.zimly.codelab.layouts.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +30,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LayoutsCodelab() {
-    Text(text = "Hi there!")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = "favorite")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text(text = "Hi there!")
+        Text(text = "This is the Layouts Codelab.")
+    }
 }
 
 @Preview
