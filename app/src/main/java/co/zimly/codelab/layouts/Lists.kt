b68836c34828d692201d3cbdbda78803466dd15f
David Bailey <davidbailey00@outlook.com>
2021-05-22 12:57:27 +0100

    Create simple list

---
+++ app/src/main/java/co/zimly/codelab/layouts/Lists.kt
@@ -0,0 +1,27 @@
+package co.zimly.codelab.layouts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SimpleList() {
+    // enable saving and manipulating the scroll state
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SimpleListPreview() {
+    SimpleList()
+}
